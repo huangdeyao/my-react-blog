0cfa6df51ec92dd62bfcc9281788f4f1a06785b1
huangdeyao <huangdeyao@markorhome.com>
2018-11-07 17:14:10 +0800

    引入antd样式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style.js';
+import './App.css';
 import './statics/iconfont/iconfont'
 
 ReactDOM.render(<App/>, document.getElementById('root'));
