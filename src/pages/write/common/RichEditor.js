9b0aba5c2e03c3441f91079b87df00f14b7b5627
huangdeyao <huangdeyao@markorhome.com>
2018-11-17 17:06:59 +0800

    新增文章

--- src/pages/write/common/RichEditor.js
+++ src/pages/write/common/RichEditor.js
@@ -1,27 +1,22 @@
 import React, {Component} from 'react';
-import RichEditorHeaderImg from './RichEditorHeaderImg';
 import {connect} from 'react-redux';
-import {LayoutMain, WriteIndexTitleInput, TextareaInput} from '../style';
+import {LayoutMain} from '../style';
 import {actionCreators} from "../../write/store";
-// 引入编辑器组件
 import BraftEditor from 'braft-editor'
-// 引入编辑器样式
 import 'braft-editor/dist/index.css'
-
 
 class RichEditor extends Component {
     render() {
-        const {titleHandleChange} = this.props;
         return (
             <LayoutMain>
-                <WriteIndexTitleInput>
-                    <TextareaInput placeholder="请输入标题（最多 50 个字）" onChange={titleHandleChange}/>
-                </WriteIndexTitleInput>
-                <BraftEditor contentStyle={{height: 'auto', minHeight: 500}}/>
+                <BraftEditor
+                    contentStyle={{height: 'auto', minHeight: 200}}
+                />
             </LayoutMain>
         )
     }
 }
+
 
 const mapState = (state) => ({});
 
@@ -31,6 +26,9 @@ const mapDispatch = (dispatch) => ({
     },
     titleHandleChange(e) {
         dispatch(actionCreators.titleHandleChange(e.target.value))
+    },
+    handlePreview() {
+
     }
 });
 
