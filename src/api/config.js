51b83a4a32bcbb0f3b899db8af140387ce2121d9
hdy <dy.huang@hotmail.com>
2018-12-06 22:38:05 +0800

    文章编辑器

--- src/api/config.js
+++ src/api/config.js
@@ -5,17 +5,18 @@
  **/
 
 const HOST = 'http://article.yrclubs.com';
-// const HOST = 'http://localhost:8770';
 const URL = {
     //获取所有文章
-    article_get_all: HOST + '/api/get/all/article',
+    article_get_all: HOST + '/storage/api/get/all/article',
     //新增
-    article_add: HOST + '/api/add/article',
+    article_add: HOST + '/storage/api/add/article',
     // 文章上传
-    article_homepage_image_upload: HOST + '/api/image/upload',
+    article_homepage_image_upload: HOST + '/storage/api/image/upload',
     // 文章上传
-    article_get_details: HOST + '/api/get/article/details?id=',
+    article_get_details: HOST + '/storage/api/get/article/details?id=',
     // 获取标签
-    article_get_tags: HOST + '/api/get/article/tags',
+    article_get_tags: HOST + '/storage/api/get/article/tags',
+    //获取所有文章
+    get_token: HOST + '/sso/oauth/token',
 };
 export default URL
--- src/pages/login/store/actionCreators.js
+++ src/pages/login/store/actionCreators.js
@@ -1,5 +1,6 @@
 import {constants} from './index';
 import axios from 'axios';
+import login from './../../../api/config';
 
 const changeLogin = () => ({
     type: constants.CHANGE_LOGIN,
@@ -13,26 +14,54 @@ export const loginOut = () => ({
 
 // axios.interceptors.request.use(
 //     config => {
-//         config.auth = {username: 'devglan-client', password: 'devglan-secret'}
-//         config.headers = {'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'}
+//         config.auth = {username: 'article-client', password: 'article-secret'};
+//         config.headers = {'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'};
 //         return config
 //     },
 //     error => {
 //         return Promise.reject(error)
 //     });
 
+// export const loginAccount = (username, password) => {
+//     return (dispatch) => {
+//         axios.get(login.get_token + '?grant_type=password&username=' + username + '&password=' + password)
+//             .then((res) => {
+//                 // const result = res.data;
+//                 if (res.status) {
+//                     dispatch(changeLogin());
+//                 } else {
+//                     alert("登录失败！");
+//                 }
+//             })
+//     }
+// };
+//
 export const loginAccount = (username, password) => {
     return (dispatch) => {
-        axios.get('http://47.94.144.123:8771/oauth/token?grant_type=password&username=' + username + '&password=' + password)
-            .then((res) => {
-                // const result = res.data;
-                if (res.status) {
-                    dispatch(changeLogin());
-                } else {
-                    alert("登录失败！");
+        // axios.interceptors.request.use(
+        //     config => {
+        //         config.auth = {username: 'devglan-client', password: 'devglan-secret'};
+        //         return config
+        //     },
+        //     error => {
+        //         return Promise.reject(error)
+        //     });
+        axios({
+                // auth: {username: 'article-client', password: 'article-secret'},
+                headers: {'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'},
+                method: 'post',
+                url: login.get_token + '?client-id=article-client&client-secret=article-secret',
+                data: {
+                    username: username,
+                    password: password,
+                    grant_type: password
                 }
-            })
+            }
+        ).then((res) => {
+            console.log(res)
+        })
     }
 };
+
 
 
