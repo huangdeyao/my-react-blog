86fadada8ba4d0ae2e4c498bdc4e3a2e587c4ffa
dy.huang@hotmail.com <dy.huang@hotmail.com>
2019-06-01 19:38:42 +0800

    修改一些配置

--- src/pages/home/common/Advertisement.js
+++ src/pages/home/common/Advertisement.js
@@ -3,20 +3,20 @@ import {
     WriterContainer,
     AdvertisementImg
 } from "./../style";
-import aliImage1 from '../../../statics/adver/alibaba.jpg';
-import aliImage3 from '../../../statics/adver/alibaba3.png';
+import aliImage1 from '../../../statics/adver/tx.jpg';
+import aliImage3 from '../../../statics/adver/al.jpg';
 
 class Advertisement extends PureComponent {
     render() {
         return (
             <div>
                 <WriterContainer>
-                    <a href='https://promotion.aliyun.com/ntms/act/enterprise-discount.html?userCode=1uif58qz'>
+                    <a href='https://cloud.tencent.com/redirect.php?redirect=1042&cps_key=67213a01a6b22c5f17736dd6857e2581&from=console'>
                         <AdvertisementImg imgUrl={aliImage1}/>
                     </a>
                 </WriterContainer>
                 <WriterContainer>
-                    <a href='https://promotion.aliyun.com/ntms/act/shoppingcart.html?userCode=1uif58qz'>
+                    <a href='https://www.aliyun.com/acts/hi-group-buying?userCode=1uif58qz'>
                         <AdvertisementImg imgUrl={aliImage3}/>
                     </a>
                 </WriterContainer>
